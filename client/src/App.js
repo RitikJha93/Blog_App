810daf9152011254f6761fc64efb199583193c89
ritik jha <coolritikjha46@gmail.com>
2023-02-12 16:57:57 +0530

    designing the create blog page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/Home";
 import Single from "./pages/Single";
+import Write from "./pages/Write";
 
 function App() {
   return <div className="App">
     <Topbar />
     {/* <Home /> */}
-    <Single />
+    {/* <Single /> */}
+    <Write />
   </div>;
 }
 
